deefeb29e184dfd058f41f4ba3222dcfdd69ae7a
a <a@a.com>
2023-04-11 23:22:33 +0800

    bug fix

--- lua/markid.lua
+++ lua/markid.lua
@@ -12,29 +12,29 @@ local markid_timer = 'markid_timer'
 local group_names = {}
 
 local string_to_int = function(str)
-    if str == nil then
-        return 0
-    end
-    local int = 0
-    for i = 1, #str do
-        local c = str:sub(i, i)
-        int = int + string.byte(c)
-    end
-    return int
+  if str == nil then
+    return 0
+  end
+  local int = 0
+  for i = 1, #str do
+    local c = str:sub(i, i)
+    int = int + string.byte(c)
+  end
+  return int
 end
 
 
 local M = {}
 local DEBUG = true
 M.colors = {
-    dark = { "#619e9d", "#9E6162", "#81A35C", "#7E5CA3", "#9E9261", "#616D9E", "#97687B", "#689784", "#999C63", "#66639C" },
-    bright = {"#f5c0c0", "#f5d3c0", "#f5eac0", "#dff5c0", "#c0f5c8", "#c0f5f1", "#c0dbf5", "#ccc0f5", "#f2c0f5", "#d8e4bc" },
-    medium = { "#c99d9d", "#c9a99d", "#c9b79d", "#c9c39d", "#bdc99d", "#a9c99d", "#9dc9b6", "#9dc2c9", "#9da9c9", "#b29dc9" }
+  dark = { "#619e9d", "#9E6162", "#81A35C", "#7E5CA3", "#9E9261", "#616D9E", "#97687B", "#689784", "#999C63", "#66639C" },
+  bright = { "#f5c0c0", "#f5d3c0", "#f5eac0", "#dff5c0", "#c0f5c8", "#c0f5f1", "#c0dbf5", "#ccc0f5", "#f2c0f5", "#d8e4bc" },
+  medium = { "#c99d9d", "#c9a99d", "#c9b79d", "#c9c39d", "#bdc99d", "#a9c99d", "#9dc9b6", "#9dc2c9", "#9da9c9", "#b29dc9" }
 }
 
 M.queries = {
-    default = "(identifier) @markid",
-    javascript = [[
+  default = "(identifier) @markid",
+  javascript = [[
           (identifier) @markid
           (property_identifier) @markid
           (shorthand_property_identifier_pattern) @markid
@@ -53,151 +53,151 @@ M.limits = {
 }
 
 function M.init()
-    ts.define_modules {
-        markid = {
-            module_path = "markid",
-            attach = function(bufnr, lang)
-                local config = configs.get_module("markid")
-                if(config.additional_vim_regex_highlighting) then
-                  vim.bo[bufnr].syntax = "ON"
+  ts.define_modules {
+    markid = {
+      module_path = "markid",
+      attach = function(bufnr, lang)
+        local config = configs.get_module("markid")
+        if (config.additional_vim_regex_highlighting) then
+          vim.bo[bufnr].syntax = "ON"
+        else
+          vim.bo[bufnr].syntax = "OFF"
+        end
+        local query = vim.treesitter.query.get(lang, 'markid')
+        if query == nil or query == '' then
+          query = vim.treesitter.query.parse(lang, config.queries[lang] or config.queries["default"])
+        end
+        local parser = parsers.get_parser(bufnr, lang)
+        local tree = parser:parse()[1]
+        local root = tree:root()
+        local delay = config.limits.delay or 100;
+
+
+        local api_get_node_text = vim.treesitter.get_node_text
+        local api_nvim_set_hl = vim.api.nvim_set_hl
+        local api_hl_range = vim.highlight.range
+
+
+
+        local highlight_tree = function(root_tree, cap_start, cap_end)
+          vim.api.nvim_buf_clear_namespace(bufnr, namespace, cap_start, cap_end)
+          local iter_count = 0
+          local max_iter = config.limits.max_iter
+          local max_col = config.limits.max_col
+          local max_textlen = config.limits.max_textlen
+          local max_names = config.limits.max_names
+          local wrap_off = config.limits.wrap_off
+          local api_node_range = nil
+
+          for id, node in query:iter_captures(root_tree, bufnr, cap_start, cap_end) do
+            iter_count = iter_count + 1
+            if iter_count > max_iter then
+              break
+            end
+            if not api_node_range then
+              api_node_range = node.range
+            end
+            local start_row, start_col, end_row, end_col = api_node_range(node)
+            if (start_col > max_col) and wrap_off then
+              vim.wo.wrap = false
+              break
+            end
+
+            local name = query.captures[id]
+            if name == "markid" then
+              local text = api_get_node_text(node, bufnr)
+              if #text > max_textlen then
+                text = text:sub(1, max_textlen)
+              end
+              if text ~= nil then
+                if hl_group_count > max_names then               -- reset count
+                  hl_group_of_identifier = {}
+                  hl_group_count = 0
+                end
+                local group_name = hl_group_of_identifier[text]
+                if group_name == nil then
+                  -- semi random: Allows to have stable global colors for the same name
+                  local colors_count = 0
+                  if not config.colors then
+                    colors_count = 0
                   else
-                  vim.bo[bufnr].syntax = "OFF"
-                end
-                local query = vim.treesitter.query.get(lang, 'markid')
-                if query == nil or query == '' then
-                  query = vim.treesitter.query.parse(lang, config.queries[lang] or config.queries["default"])
-                end
-                local parser = parsers.get_parser(bufnr, lang)
-                local tree = parser:parse()[1]
-                local root = tree:root()
-                local delay = config.limits.delay or 100;
+                    colors_count = #config.colors
+                  end
+                  if colors_count == 0 then
+                    return
+                  end
 
-
-                local api_get_node_text = vim.treesitter.get_node_text
-                local api_nvim_set_hl = vim.api.nvim_set_hl
-                local api_hl_range = vim.highlight.range
-
-
-
-                local highlight_tree = function(root_tree, cap_start, cap_end)
-                    vim.api.nvim_buf_clear_namespace(bufnr, namespace, cap_start, cap_end)
-                    local iter_count = 0
-                    local max_iter = config.limits.max_iter
-                    local max_col = config.limits.max_col
-                    local max_textlen = config.limits.max_textlen
-                    local max_names = config.limits.max_names
-                    local wrap_off =config.limits.wrap_off
-                    local api_node_range = nil
-
-                    for id, node in query:iter_captures(root_tree, bufnr, cap_start, cap_end) do
-                        iter_count = iter_count + 1
-                        if iter_count > max_iter then
-                            break
-                        end
-                        if not api_node_range then
-                          api_node_range = node.range
-                        end
-                        local start_row, start_col, end_row, end_col =api_node_range(node)
-                        if (start_col > max_col) and wrap_off then
-                            vim.wo.wrap = false
-                            break
-                        end
-
-                        local name = query.captures[id]
-                        if name == "markid" then
-                            local text = api_get_node_text(node, bufnr)
-                            if #text > max_textlen then
-                              text = text:sub(1, max_textlen)
-                            end
-                            if text ~= nil then
-                              if hl_group_count > max_names then -- reset count
-                                hl_group_of_identifier = {}
-                                hl_group_count = 0
-                              end
-                                local group_name = hl_group_of_identifier[text]
-                                if group_name == nil then
-                                    -- semi random: Allows to have stable global colors for the same name
-                                    local colors_count = 0
-                                    if not config.colors then
-                                        colors_count = 0
-                                    else
-                                      colors_count = #config.colors
-                                    end
-                                    if colors_count == 0 then
-                                      return
-                                    end
-
-                                    hl_index=hl_index+1
-                                    if (hl_index>colors_count) then
-                                      hl_index = 1
-                                    end
-                                    local idx = hl_index
-                                    if #group_names ==0 then
-                                       for i = 1, colors_count, 1 do
-                                        group_names[i] = "markid" .. i
-                                       end
-                                    end
-                                    group_name = group_names[idx]
-                                    if config.colors then
-                                      api_nvim_set_hl(0, group_name, { default = true, fg = config.colors[idx] })
-                                    end
-                                    hl_group_of_identifier[text] = group_name
-                                    hl_group_count = hl_group_count + 1
-                                end
-                                if group_name ~= nil then
-                                  local range_start = {start_row, start_col}
-                                  local range_end = {end_row, end_col}
-                                  api_hl_range(
-                                      bufnr,
-                                      namespace,
-                                      group_name,
-                                      range_start,
-                                      range_end
-                                  )
-                                end
-                            end
-                        end
+                  hl_index = hl_index + 1
+                  if (hl_index > colors_count) then
+                    hl_index = 1
+                  end
+                  local idx = hl_index
+                  if #group_names == 0 then
+                    for i = 1, colors_count, 1 do
+                      group_names[i] = "markid" .. i
                     end
+                  end
+                  group_name = group_names[idx]
+                  if config.colors then
+                    api_nvim_set_hl(0, group_name, { default = true, fg = config.colors[idx] })
+                  end
+                  hl_group_of_identifier[text] = group_name
+                  hl_group_count = hl_group_count + 1
                 end
+                if group_name ~= nil then
+                  local range_start = { start_row, start_col }
+                  local range_end = { end_row, end_col }
+                  api_hl_range(
+                    bufnr,
+                    namespace,
+                    group_name,
+                    range_start,
+                    range_end
+                  )
+                end
+              end
+            end
+          end
+        end
 
-                highlight_tree(root, 0, -1)
-                parser:register_cbs(
-                    {
-                        on_changedtree = function(changes, tree)
-                          if false then
-                            highlight_tree(tree:root(), 0, -1) -- can be made more efficient, but for plain identifier changes, `changes` is empty
-                          else
-                            local oldtimer = vim.api.nvim_buf_get_var(bufnr, markid_timer)
-                            if oldtimer then
-                              vim.fn.timer_stop(oldtimer)
-                            end
-                            oldtimer = vim.fn.timer_start(bufnr, delay, function()
-                              vim.api.nvim_buf_del_var (bufnr, markid_timer)
-                              highlight_tree(tree:root(), 0, -1)
-                            end)
-                            vim.api.nvim_buf_set_var(bufnr, "markid_timer", oldtimer)
-                          end
-                        end
-                    }
-                )
-            end,
-            detach = function(bufnr)
-                vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
+        highlight_tree(root, 0, -1)
+        parser:register_cbs(
+          {
+            on_changedtree = function(changes, tree)
+              if false then
+                highlight_tree(tree:root(), 0, -1)             -- can be made more efficient, but for plain identifier changes, `changes` is empty
+              else
                 local oldtimer = vim.api.nvim_buf_get_var(bufnr, markid_timer)
                 if oldtimer then
                   vim.fn.timer_stop(oldtimer)
-                  vim.api.nvim_buf_del_var(bufnr, markid_timer)
                 end
-            end,
-            is_supported = function(lang)
-                local queries = configs.get_module("markid").queries
-                return pcall(vim.treesitter.query.parse, lang, queries[lang] or queries["default"])
-            end,
-            colors = M.colors.medium,
-            queries = M.queries,
-            limits = M.limits
-        }
+                oldtimer = vim.fn.timer_start(bufnr, delay, function()
+                  vim.api.nvim_buf_del_var(bufnr, markid_timer)
+                  highlight_tree(tree:root(), 0, -1)
+                end)
+                vim.api.nvim_buf_set_var(bufnr, markid_timer, oldtimer)
+              end
+            end
+          }
+        )
+      end,
+      detach = function(bufnr)
+        vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
+        local oldtimer = vim.api.nvim_buf_get_var(bufnr, markid_timer)
+        if oldtimer then
+          vim.fn.timer_stop(oldtimer)
+          vim.api.nvim_buf_del_var(bufnr, markid_timer)
+        end
+      end,
+      is_supported = function(lang)
+        local queries = configs.get_module("markid").queries
+        return pcall(vim.treesitter.query.parse, lang, queries[lang] or queries["default"])
+      end,
+      colors = M.colors.medium,
+      queries = M.queries,
+      limits = M.limits
     }
+  }
 end
 
 return M
