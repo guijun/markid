20627bb77779e49f9f9574b535bfbc6015a44664
a <a@a.com>
2023-04-21 14:17:57 +0800

    TSPrebuild is ok ?

--- lua/markid.lua
+++ lua/markid.lua
@@ -23,6 +23,48 @@ local VISIBLE_MIN_HEIGHT = 200
 local modulename = "markid"
 local namespace = vim.api.nvim_create_namespace(modulename)
 
+local TSPrebuild = {}
+has_prebuilt = false
+
+TSPrebuild.on_attach = function(client, bufnr)
+  if has_prebuilt then return end
+
+  local query = require("vim.treesitter.query")
+
+  local function safe_read(filename, read_quantifier)
+    local file, err = io.open(filename, "r")
+    if not file then error(err) end
+    local content = file:read(read_quantifier)
+    io.close(file)
+    return content
+  end
+
+  local function read_query_files(filenames)
+    local contents = {}
+
+    for _, filename in ipairs(filenames) do table.insert(contents, safe_read(filename, "*a")) end
+
+    return table.concat(contents, "")
+  end
+
+  local function prebuild_query(lang, query_name)
+    local query_files = query.get_files(lang, query_name)
+    local query_string = read_query_files(query_files)
+
+    query.set(lang, query_name, query_string)
+  end
+
+  local prebuild_languages = { "typescript", "javascript" }
+  for _, lang in ipairs(prebuild_languages) do
+    prebuild_query(lang, "highlights")
+    prebuild_query(lang, "injections")
+  end
+
+  has_prebuilt = true
+end
+
+
+
 -- Global table to store names of created highlight groups
 local hl_group_of_identifier = {}
 local hl_group_count = 0
@@ -412,6 +454,7 @@ function M.init()
     markid = {
       module_path = modulename,
       attach = function(bufnr, lang)
+        TSPrebuild.on_attach(bufnr, lang)
         MarkId_Clear(bufnr)
 
         MarkIdBufStatus_HL_Clear(bufnr)
